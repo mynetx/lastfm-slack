95c8c69b2f28e70c1b7b89a52c87599b0b0df9e2
Joachim Rütter <jm@pixelkode.com>
2023-10-31 09:54:28 +0100

    Refactor code

--- statusUpdate.php
+++ statusUpdate.php
@@ -17,60 +17,55 @@ function authenticateAndGetSessionKey()
 {
     $sessionKey = $_ENV['LASTFM_SESSION_KEY'] ?? null;
 
-    if (empty($sessionKey)) {
-        // Get the Last.fm API key and secret from the environment
-        $apiKey = $_ENV['LASTFM_KEY'];
-        $apiSecret = $_ENV['LASTFM_SECRET'];
-
-        // Step 1: Get a token
-        $client = new Client();
-        $response = $client->get("http://ws.audioscrobbler.com/2.0/?method=auth.gettoken&api_key=$apiKey&format=json");
-        $data = json_decode($response->getBody());
-        $token = $data->token;
-
-        // Step 2: Open the authorization URL in your browser to authorize the application
-        $authUrl = "http://www.last.fm/api/auth/?api_key=$apiKey&token=$token";
-        echo "Visit this URL in your browser and authorize the application: $authUrl" . PHP_EOL;
-
-        // Step 3: After authorization, enter the token you received to get the session key
-        echo "Enter the token you received after authorizing the application: ";
-        $enteredToken = trim(fgets(STDIN));
-
-        if ($enteredToken === $token) {
-            $client = new Client();
-            $data = [
-                "api_key=$apiKey",
-                "method=auth.getSession",
-                "token=$token"
-            ];
-            $data[] = "api_sig=" . md5(str_replace('=', '', implode('', $data)) . $apiSecret);
-            $data[] = "format=json";
-            $response = $client->get("http://ws.audioscrobbler.com/2.0/?" . implode("&", $data));
-
-            if ($response->getStatusCode() === 200) {
-                $body = $response->getBody()->getContents();
-                $data = json_decode($body, true);
-
-                if (isset($data['session']['key'])) {
-                    $sessionKey = $data['session']['key'];
-                    file_put_contents('.env', "LASTFM_SESSION_KEY=$sessionKey" . PHP_EOL, FILE_APPEND);
-                    echo "Your session key has been stored in the .env file." . PHP_EOL;
-                    return $data['session']['key'];
-                } else {
-                    echo "Failed to obtain a session key. Authentication failed." . PHP_EOL;
-                    exit(1);
-                }
-            } else {
-                echo "Failed to obtain a session key. Authentication failed." . PHP_EOL;
-                exit(1);
-            }
-        } else {
-            echo "Tokens do not match. Authentication failed." . PHP_EOL;
-            exit(1);
-        }
+    if (!empty($sessionKey)) {
+        return $sessionKey;
     }
 
+    $apiKey = $_ENV['LASTFM_KEY'];
+    $apiSecret = $_ENV['LASTFM_SECRET'];
+
+    $token = getToken($apiKey);
+    $sessionKey = getSessionKey($apiKey, $apiSecret, $token);
+
+    if (empty($sessionKey)) {
+        echo "Failed to obtain a session key. Authentication failed." . PHP_EOL;
+        exit(1);
+    }
+
+    file_put_contents('.env', "LASTFM_SESSION_KEY=$sessionKey" . PHP_EOL, FILE_APPEND);
+    echo "Your session key has been stored in the .env file." . PHP_EOL;
+
     return $sessionKey;
+}
+
+function getToken($apiKey)
+{
+    $client = new Client();
+    $response = $client->get("http://ws.audioscrobbler.com/2.0/?method=auth.gettoken&api_key=$apiKey&format=json");
+    $data = json_decode($response->getBody());
+    return $data->token;
+}
+
+function getSessionKey($apiKey, $apiSecret, $token)
+{
+    $data = [
+        "api_key=$apiKey",
+        "method=auth.getSession",
+        "token=$token",
+    ];
+    $data[] = "api_sig=" . md5(str_replace('=', '', implode('', $data)) . $apiSecret);
+    $data[] = "format=json";
+
+    $client = new Client();
+    $response = $client->get("http://ws.audioscrobbler.com/2.0/?" . implode("&", $data));
+
+    if ($response->getStatusCode() === 200) {
+        $body = $response->getBody()->getContents();
+        $data = json_decode($body, true);
+        return $data['session']['key'] ?? null;
+    }
+
+    return null;
 }
 
 // Function to retrieve the last track listened to by the user
@@ -103,18 +98,26 @@ function getTrackInfo()
 // Function to update Slack status
 function updateSlackStatus($status, $trackName = '', $trackArtist = '')
 {
-    $emoji = '';
+    $statusText = '';
+    $statusEmoji = '';
 
     if ($status !== '') {
         echo $status . PHP_EOL;
         $emoji = (new Emoji())->get($trackName, $trackArtist);
+        $statusText = $status;
+        $statusEmoji = $emoji;
     }
 
     $slackTokens = [
         $_ENV['SLACK_TOKEN_1'],
-        $_ENV['SLACK_TOKEN_2']
+        $_ENV['SLACK_TOKEN_2'],
     ];
 
+    updateStatusInSlack($statusText, $statusEmoji, $slackTokens);
+}
+
+function updateStatusInSlack($statusText, $statusEmoji, $slackTokens)
+{
     $client = new Client();
     try {
         foreach ($slackTokens as $token) {
@@ -122,8 +125,8 @@ function updateSlackStatus($status, $trackName = '', $trackArtist = '')
                 'form_params' => [
                     'token' => $token,
                     'profile' => json_encode([
-                        'status_text' => $status, // Set status_text to empty string when no music is playing
-                        'status_emoji' => $emoji,  // Set status_emoji to empty string when no music is playing
+                        'status_text' => $statusText,
+                        'status_emoji' => $statusEmoji,
                     ]),
                 ],
             ]);
@@ -168,8 +171,6 @@ function getSlackStatus(&$currentStatus)
     }
 }
 
-$currentStatus = '';
-
 // Initialize the script
 function init()
 {
@@ -178,11 +179,13 @@ function init()
     getSlackStatus($currentStatus);
 }
 
+$currentStatus = ''; // Initialize currentStatus to an empty string
+
 // Main loop
 while (true) {
     if ($_ENV['RESTART'] === 'true') {
-        // Initialize the script
-        init();
+        $currentStatus = ''; // Reset status to nothing
+        getSlackStatus($currentStatus);
         echo "Restarted." . PHP_EOL;
     }
     sleep(10); // Sleep for 10 seconds
